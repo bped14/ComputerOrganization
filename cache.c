b78282166f6bab6f513365e0f01d59a2c757bc70
bped14 <brpe0567@colorado.edu>
2017-04-14 12:01:32 -0600

    comments to cache.c

--- cache.c
+++ cache.c
@@ -7,6 +7,7 @@
 
 //Structs (Cache and Block)
 struct Block_ {
+  //valid and dirty are not part of the block address
   int valid;
   int tag;
   int dirty;
@@ -24,6 +25,7 @@ struct Cache_ {
   Block* blocks;
 };
 
+//create a cache (I cache and D cache)
 Cache CreateCache(int cache_size, int block_size, int write_policy)
 {
   //local variables
@@ -65,4 +67,51 @@ Cache CreateCache(int cache_size, int block_size, int write_policy)
   cache->cache_size = CACHE_SIZE;
   cache->block_size = BLOCK_SIZE;
   cache->lines = (int)(CACHE_SIZE/BLOCK_SIZE);
+  cache->blocks = (BLock*) malloc(sizeof(Block) * cache->lines);
+  assert(cache->blocks != NULL);
+
+  //initialize cache
+  //fill valid and dirty bits with zero
+
+  for(i=0; i<cache->lines; i++)
+  {
+    cache->blocks[i] = (Block) malloc(sizeof(struct Block_));
+    assert(cache->blocks[i] != NULL);
+    cache->blocks[i]->valid = 0;
+    cache->blocks[i]->dirty = 0;
+    cache->blocks[i]->tag = NULL;
+  }
+
+  return cache;
 }
+
+/*how professor implemented
+
+i-cache_read(takes in address, &data)
+{
+if (address & tag mask(bit mask)) == tag[blockaddress]) && (valid[blockaddress])
+{
+then if true we have cache hits
+&data = icache[cacheaddress];
+cachehit = true;
+}
+//must have cache miss
+else
+{
+cachehit = false;
+valid[blockaddress] = false;
+//call main memory to get data
+datavalid = main_memory(address, &data) //include penalty of 8 cycles mainmemory count down from 8, when = 0 change to hit
+if(datavalid == true)
+{
+cachedata[cache address] = data;
+//when valid
+update valid bit
+}
+return cachehit; if hit tell pipe data was valid, if false stall pipeline
+}
+}
+
+//do these before you enter i-cache_read
+block address = [(address >> byte offset >> block offset) & block mask)]
+cache address = block address << block offset + block offset value)
